a1f417902a7807d5c0dd6b63ac58e2ac8d98748c
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-10-16 09:18:08 -0400

    Disable serving static files from the  folder by default since Apache or NGINX already handles this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "codegirlco_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "blog_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -75,8 +75,10 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-# Replace gem rails_12factor to enable static asset serving and logging on Heroku with appropriate configuration
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
