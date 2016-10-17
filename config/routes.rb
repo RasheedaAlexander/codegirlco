a53b33a5bebbf7e0fdd546635a14843639b8bbf0
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-10-17 06:10:20 -0400

    creates seven different routes in your application, all mapping to the Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # creates seven different routes in your application, all mapping to the Posts controller
+  resources :posts
   root to: 'posts#index'
 end
