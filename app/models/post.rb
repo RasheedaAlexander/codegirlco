10e004dcc8ea076dfc1accf49437a77ba4861c84
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-12-28 16:21:33 -0500

    syntax error fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 	extend FriendlyId
   friendly_id :title, use: :slugged
-  def should_generate_new_friedly_id?
+  def should_generate_new_friendly_id?
      slug.nil? || title_changed?
   end﻿
 end
